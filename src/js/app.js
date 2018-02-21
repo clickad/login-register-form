2ca068d19a1bf2d536230ccd3fa4b84b2827e90f
Adnan Damadzic <adnan.damadzic@gmail.com>
2018-02-21 14:19:13 +0430

    responsive height fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,18 +5,18 @@
       var self = this;
       this.$window = $(window);
       this.$wrapper = $('.wrapper');
+      this.$formWrapper = $(".form-container");
       this.$form = $('.form');
       this.$defaultForm = $(".form-wrapper:first");
       this.$password = $("#password"); 
       this.$confirmPassword = $("#confirm-password");
       this.$tabcontent = $(".form-wrapper");
       this.$link = $('.nav_link');
-
+      this.$formWrapper.show();
       setTimeout(function(){
         self.$defaultForm.fadeIn('slow');
       });
 
-      this.$window.on('resize', this.fixHeight.bind(this));
       this.$link.on('click', this.openForm.bind(this));
       this.$password.on('change', this.validatePassword.bind(this));
       this.$confirmPassword.on('keyup', this.validatePassword.bind(this));
@@ -32,12 +32,6 @@
       var index = formToHide.index();
       formToHide.hide();
       index === 0 ? $(this.$tabcontent[1]).show() : $(this.$tabcontent[0]).show();
-      this.fixHeight();
-    },
-    fixHeight: function(){ // Cannot use min-height on wrapper because of ie bug with flex box
-      var wrapperHeight = this.$wrapper.height();
-      var formHeight = this.$form.height();
-      wrapperHeight < formHeight ? this.$wrapper.height(formHeight) : this.$wrapper.height('100%');
     }
   };
 
